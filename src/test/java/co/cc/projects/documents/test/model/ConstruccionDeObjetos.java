7d811421572ca5addfe21d186f36344afcfff487
anothererk32 <another.cristiancastillo32@gmail.com>
2019-01-11 17:55:55 -0500

    Spaces only

--- src/test/java/co/cc/projects/documents/test/model/ConstruccionDeObjetos.java
+++ src/test/java/co/cc/projects/documents/test/model/ConstruccionDeObjetos.java
@@ -10,22 +10,22 @@ import static co.cc.projects.documents.persistence.model.Usuario.*;
 
 public class ConstruccionDeObjetos {
 
-	@Test
-	public void crearUsuarioConMuchosParametros() {
-		String numeroDocumento = "1234567";
-		String tipoDocumento = "CC";
-		String nombres = "ALEBERTO";
-		String apellidos = "GARCIA";
-		String email = "some-email@email.com";
-		boolean estaActivo = false;
-		
-		Usuario usuario = new UsuarioBuilder(new DocumentoIdentidad(numeroDocumento, tipoDocumento), nombres, apellidos)
-											.activo(estaActivo)
-											.email(email)
-											.build();
-		assertEquals(usuario.getDocumentoIdentidad().getTipoIdentificacion(), tipoDocumento);
-		assertEquals(usuario.getEmail(), email);
-		assertEquals(usuario.isActivo(), estaActivo);
-	}
-	
+    @Test
+    public void crearUsuarioConMuchosParametros() {
+        String numeroDocumento = "1234567";
+        String tipoDocumento = "CC";
+        String nombres = "ALEBERTO";
+        String apellidos = "GARCIA";
+        String email = "some-email@email.com";
+        boolean estaActivo = false;
+
+        Usuario usuario = new UsuarioBuilder(new DocumentoIdentidad(numeroDocumento, tipoDocumento), nombres, apellidos)
+                                            .activo(estaActivo)
+                                            .email(email)
+                                            .build();
+        assertEquals(usuario.getDocumentoIdentidad().getTipoIdentificacion(), tipoDocumento);
+        assertEquals(usuario.getEmail(), email);
+        assertEquals(usuario.isActivo(), estaActivo);
+    }
+
 }
